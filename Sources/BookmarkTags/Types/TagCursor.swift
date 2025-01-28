74441ec71a456f0989f5fe1863ddc2fae143db35
Joseph Wardell <jay@jaywardell.me>
2025-01-28 00:06:22 -0500

    added access control

--- Sources/BookmarkTags/Types/TagCursor.swift
+++ Sources/BookmarkTags/Types/TagCursor.swift
@@ -10,13 +10,13 @@ import SwiftUICore
 public struct TagsCursor {
     let tags: [TagInfo]
     
-    var isEmpty: Bool { tags.isEmpty }
+    public var isEmpty: Bool { tags.isEmpty }
     
     private init(tags: [TagInfo]) {
         self.tags = tags
     }
     
-    func contains(_ tag: TagInfo) -> Bool {
+    public func contains(_ tag: TagInfo) -> Bool {
         tags.contains(tag)
     }
     
@@ -24,13 +24,13 @@ public struct TagsCursor {
         .init(tags: [])
     }
     
-    func adding(_ tag: TagInfo) -> Self {
+    public func adding(_ tag: TagInfo) -> Self {
         guard !tags.contains(tag) else { return self }
         
         return .init(tags: tags + [tag])
     }
     
-    func removing(_ tag: TagInfo) -> Self {
+    public func removing(_ tag: TagInfo) -> Self {
         var newTags = tags
         if let index = tags.firstIndex(where: { $0 == tag }) {
             newTags.remove(at: index)
